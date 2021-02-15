1ddce934ade76218e88b592ec49d9b233ebdd4b4
Harley Latsky <harley@latsky.com>
2021-02-15 13:14:59 -0500

    Added Docstrings + Cleanup

--- tools/dataCreation.py
+++ tools/dataCreation.py
@@ -3,39 +3,31 @@ from pydub.silence import split_on_silence
 import speech_recognition as sr
 import wave
 import os
-import shutil
 from playsound import playsound
 import pyaudio
 from time import sleep
 import shutil
+from tools.wordSeparation import clear_audio
+
+directory_to_project = dir_path = os.path.dirname(os.path.realpath(__file__))
 
 
-directory_to_project = dir_path = os.path.dirname(os.path.realpath(__file__))  # "C:\\Users\\harle\\PycharmProjects\\QMINDv4"
+def create_recorded_data(directory, seconds):
+    """
+    Helper function to record audio (Actually records it and places into collected recordings)
+    Args:
+        directory: str
+            The word that the person is saying multiple times
+        seconds: int
+            Amount of time given to record. (Usually 7-8 seconds)
 
-# What do you want the program to do?
-# record_new_data = False  # Records all data from your microphone
-# break_up_audio = False  # Breaks up the recorded data to separate audio files
-# play_recordings = True  # Plays you broken up audio files!
-
-
-def clear_audio(path):  # Helper Function to Empty Directory of Useless Files
-    for filename in os.listdir(path):
-        file_path = os.path.join(path, filename)
-        try:
-            if os.path.isfile(file_path) or os.path.islink(file_path):
-                os.unlink(file_path)
-            elif os.path.isdir(file_path):
-                shutil.rmtree(file_path)
-        except Exception as e:
-            print('Failed to delete %s. Reason: %s' % (file_path, e))
-
-
-def create_recorded_data(directory, filename, seconds):  # Helper function to record audio
+    Returns: NULL
+    """
     chunk = 1024  # Record in chunks of 1024 samples
     sample_format = pyaudio.paInt16  # 16 bits per sample
     channels = 2
     fs = 44100  # Record at 44100 samples per second
-    filename = f"collectedRecordings\\rawRecordings\\{directory}_{filename}"
+    filename = f"collectedRecordings\\rawRecordings\\{directory}_RecordedData.wav"
     print(filename)
     p = pyaudio.PyAudio()  # Create an interface to PortAudio
 
@@ -83,15 +75,22 @@ directories = ["the", "of", "and", "to", "be"]
 
 
 def record_new_data():
+    """
+    Records all new data by calling the create recorded data function for each word we are training on
+    """
     print("Get ready to record audio input")
     print("Wait ~0.5 seconds between each utterance of the word")
     print("You have 8 seconds for each, stay silent after you are done saying 5 utterances")
     sleep(5)
     for directory in directories:
-        create_recorded_data(directory, sound_file_name, 8)
+        create_recorded_data(directory, 8)
 
 
-def break_up_audio(dir=dir_path):  # Break up the audio into separate word files
+def break_up_audio():  # Break up the audio into separate word files
+    """
+    Breaks up the audio into separate word files to process and prints out a guess as to what the audio file says
+    Returns: NULL
+    """
     for directory in directories:
         sound_file = AudioSegment.from_wav(f"collectedRecordings\\rawRecordings\\{directory}_RecordedData.wav")
         # min_silence_len=time in milliseconds, silence_thresh=what the comp classifies as silence in dB
@@ -106,19 +105,34 @@ def break_up_audio(dir=dir_path):  # Break up the audio into separate word files
             numWords += 1
         print(f"Exported {numWords} .wav files")
 
-        # CONVERT AUDIO TO TEXT ------------- cool lil feature
-        # initialize the recognizer
-        r = sr.Recognizer()
+        audio_to_text(f"collectedRecordings\\rawRecordings\\{directory}_RecordedData.wav")  # converts audio to text
 
-        # open the file
-        with sr.AudioFile(f"collectedRecordings\\rawRecordings\\{directory}_RecordedData.wav") as source:
-            # listen for the data (load audio to memory)
-            audio_data = r.record(source)
-            # recognize (convert from speech to text)
-            text = r.recognize_google(audio_data)
-            print(text)
 
-def play_recordings():  # A test feature to test segments
+def audio_to_text(audio_dir):
+    """
+    Transcribes an audio file (Prints out the audio to text)
+    Args:
+        dir: str
+            Path to where the audio file is
+    Returns: NULL
+    """
+    # initialize the recognizer
+    r = sr.Recognizer()
+
+    # open the file
+    with sr.AudioFile(audio_dir) as source:
+        # listen for the data (load audio to memory)
+        audio_data = r.record(source)
+        # recognize (convert from speech to text)
+        text = r.recognize_google(audio_data)
+        print(text)
+
+
+def play_recordings():
+    """
+    Plays all of the recordings back to user to ensure that they were recorded correctly (A test feature)
+    Returns: NULL
+    """
     for directory in directories:
         numFiles = os.listdir(f"collectedRecordings\\{directory}")
         print(f"Words in \"{directory}\" directory")
@@ -130,7 +144,8 @@ def play_recordings():  # A test feature to test segments
 
 
 def export_recordings(name="foo"):
-    """copy files from collected recordings to /words/'word'/foo/
+    """
+    copy files from collected recordings to /words/'word'/foo/
     """
     for directory in directories:
         numFiles = os.listdir(f"collectedRecordings\\{directory}")
--- tools/wordSeparation.py
+++ tools/wordSeparation.py
@@ -1,30 +1,43 @@
 from pydub import AudioSegment
 from pydub.silence import split_on_silence
 import speech_recognition as sr
-import wave
-from pydub.playback import play
 import os
 import shutil
 from playsound import playsound
-import pyaudio
 
 
-def clear_audio(path):  # Empty Directory
-    num_del_words = 0
+def clear_audio(path):
+    """
+    Empties a specified directory
+    Args:
+        path: str
+            specified directory
+
+    Returns: NULL
+
+    """
+
     for filename in os.listdir(path):
         file_path = os.path.join(path, filename)
         try:
             if os.path.isfile(file_path) or os.path.islink(file_path):
                 os.unlink(file_path)
-                num_del_words += 1
             elif os.path.isdir(file_path):
                 shutil.rmtree(file_path)
         except Exception as e:
             print('Failed to delete %s. Reason: %s' % (file_path, e))
-    print(f"Deleted {num_del_words} previous .wav files")
 
 
 def separate_words(sound_file_name):
+    """
+    Separates an audio into constituent words and places them into splitAudio folder
+    Args:
+        sound_file_name: str
+            Name of the audio file
+
+    Returns: NULL
+
+    """
     sound_file = AudioSegment.from_wav(sound_file_name)
     # min_silence_len=time in milliseconds, silence_thresh=what the comp classifies as silence in dB
     audio_chunks = split_on_silence(sound_file, min_silence_len=50, silence_thresh=(int(sound_file.dBFS) - 10))
